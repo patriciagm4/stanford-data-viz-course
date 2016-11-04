7d382780b6a7f0cde164dc7f673dfdc731fd2d7a
Patricia Gonzales <patgonza@DN51stbp.SUNet>
2016-11-03 21:03:11 -0700

    dc main

--- app/assets/js/dc_visualization/main.js
+++ app/assets/js/dc_visualization/main.js
@@ -1,15 +1,5 @@
-console.warn("------------------- dc_visualization/main.js START -------------------");
-/**
-    * Action Required: change the data URL to your own Heroku URL
-    * example: http://data.datavizcourse.com/api/v1.0/data/iris/ ---> http://punctual-cheesecake-5159.herokuapp.com/api/v1.0/data/iris/
-    * make sure you use HTTP and not HTTPS
-**/
 
-var dc_data_url = "http://data.datavizcourse.com/api/v1.0/data/iris/";
-
-console.warn("Action Required: In dc_visualization/main.js line 10, change the 'dc_data_url' variable to your own Heroku Application's URL.");
-
-/* end action required section */
+var dc_data_url = "http://floating-retreat-19012.herokuapp.com/api/v1.0/data/iris/";
 
 
 // ------------------------------------------------------------------
@@ -27,41 +17,19 @@ d3.json(dc_data_url, function(remote_json){
   // define dimensions for each variable in the iris dataset, and round each dimension's value to the nearest .5
 
   var dim_sepal_length  = cf.dimension(function(d){ return Math.round(d.sepal_length * 2)/2; });
-
-  /**
-      * Action Required: define accessor functions for each dimension 
-      * hint: dim_sepal_length is done for you, the others look the same (except dim_species)
-      * freebie: var dim_species = cf.dimension(function(d){ return d.species; });
-  **/
-  
-  var dim_sepal_width   = null;
-  var dim_petal_length  = null;
-  var dim_petal_width   = null;
-  var dim_species       = null;
-
-  console.warn("Action Required: In dc_visualization/main.js lines 37-40, change null to an accessor function for each dimension.")
-
-  /* end action required section */
+  var dim_sepal_width   = cf.dimension(function(d){return Math.round(d.sepal_width * 2)/2;});
+  var dim_petal_length  = cf.dimension(function(d){return Math.round(d.petal_length * 2)/2;});
+  var dim_petal_width   = cf.dimension(function(d){return Math.round(d.petal_width * 2)/2;});
+  var dim_species       = cf.dimension(function(d){ return d.species;
 
   // ------------------------------------------------------------------
   // define grouping function for each dimension
 
   var group_sum_sepal_length = dim_sepal_length.group().reduceSum(function(d){ return d.sepal_length; });
- 
-  /**
-      * Action Required: define grouping functions for each dimension 
-      * hint: group_sum_sepal_length is done for you, the others look the same (except group_count_species)
-      * freebie: var group_count_species = dim_species.group().reduceCount();
-  **/
-
-  var group_sum_sepal_width  = null;
-  var group_sum_petal_length = null;
-  var group_sum_petal_width  = null;
-  var group_count_species    = null;
-
-  console.warn("Action Required: In dc_visualization/main.js lines 57-60, change null to  groups on the dimensions.")
-
-  /* end action required section */
+  var group_sum_sepal_width  = sepal_width.group().reduceSum(function(d){return d.sepal_width; });
+  var group_sum_petal_length = petal_length.group().reduceSum(function(d){return d.petal_length; });
+  var group_sum_petal_width  = petal_width.group().reduceSum(function(d){return d.petal_width; });
+  var group_count_species    = dim_species.group().reduceCount();
 
   // ------------------------------------------------------------------
   // get unique species (used in the species chart below)
@@ -98,30 +66,55 @@ d3.json(dc_data_url, function(remote_json){
           .x(d3.scale.ordinal().domain(species_names))
           .xUnits(dc.units.ordinal)
   **/
-
-  var sepal_width_chart = dc
-    // .barChart("#sepal_width_chart")
+  var species_chart = dc
+    .barChart("#species_chart")
+    .width(240)
+    .height(200)
+    .dimension(species)
+    .group(species_sum)
+    .centerBar(true)
+    .x(d3.scale.ordinal().domain(species_names))
+    .xUnits(dc.units.ordinal);
     
+  var sepal_length_chart = dc
+    .barChart("#sepal_length_chart")
+    .width(240)
+    .height(200)
+    .dimension(sepal_length)
+    .group(sepal_length_sum)
+    .centerBar(true)
+    .x( d3.scale.linear().domain([3,10]) )
+    .xUnits(dc.units.fp.precision(.5));
+  
+  var sepal_width_chart = dc
+    .pieChart("#sepal_width_chart")
+  	.width(240)
+  	.height(200)
+  	.radius(100)
+  	.innerRadius(50)
+  	.dimension(sepal_width)
+  	.group(sepal_width_sum)   
+  	.renderLabel(true);
+
   var petal_length_chart = dc
-    //.barChart("#petal_length_chart")
+    .barChart("#petal_length_chart")
+    .width(240)
+    .height(200)
+    .dimension(petal_length)
+    .group(petal_length_sum)
+    .centerBar(true)
+    .x( d3.scale.linear().domain([0,8]) )
+    .xUnits(dc.units.fp.precision(0.5));
 
   var petal_width_chart = dc
-    //.barChart("#petal_width_chart")
-
-  var species_chart = dc
-    //.barChart("#species_chart")
-
-  console.warn("Action Required: In dc_visualization/main.js lines 102-112, finish building the charts.")
-
-  /* end action required section */
-
-  
-  /**  
-  ==================================================================
-   No Further Action Required: the rest is provided for you 
-  ================================================================== 
-  **/
-
+    .pieChart("#petal_width_chart")
+  	.width(240)
+  	.height(200)
+  	.radius(100)
+  	.innerRadius(50)
+  	.dimension(petal_width)
+  	.group(petal_width_sum)
+  	.renderLabel(true);
 
   
   // ------------------------------------------------------------------
